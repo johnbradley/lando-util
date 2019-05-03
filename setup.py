81e57e92d233b729aa8f05035f69319d8bb2f5a5
John Bradley <johnbradley2008@gmail.com>
2019-05-03 08:57:53 -0400

    first pass at circleci orb deploy to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
+import os
+import sys
 from setuptools import setup
+# version checking derived from https://github.com/levlaz/circleci.py/blob/master/setup.py
+from setuptools.command.install import install
+
+VERSION = '0.4.0'
+TAG_ENV_VAR = 'CIRCLE_TAG'
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv(TAG_ENV_VAR)
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 
 setup(name='lando-util',
-      version='0.4.0',
+      version=VERSION,
       description='Utility functions for https://github.com/Duke-GCB/lando',
       url='http://github.com/Duke-GCB/lando-util',
       license='MIT',
@@ -15,4 +37,8 @@ setup(name='lando-util',
           'Markdown==2.6.9',
           'PyYAML==5.1',
       ],
-      zip_safe=False)
+      zip_safe=False,
+      cmdclass={
+          'verify': VerifyVersionCommand,
+      },
+      )
